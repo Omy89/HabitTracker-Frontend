809291e2f26fce42acf6b3e1f5a363499c693fee
Omy89 <oromerom@unitec.edu>
2026-09-06 13:57:20 -0600

    cambios minmos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import Image from 'next/image';
+import AppBar from './components/layout/AppBar';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+    <div className="flex flex-col flex-1 items-center font-sans">
+      <AppBar />
       <h1 className="text-6xl font-bold text-black">Prueba de Next.js 13</h1>
       <p className="mt-6 text-2xl text-black">
-        Esta es una prueba de Next.js 13 con TypeScript y Tailwind CSS.
+        no he avanzado nada de el proyecto
       </p>
     </div>
   );
